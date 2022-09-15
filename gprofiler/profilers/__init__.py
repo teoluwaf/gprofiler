405e170d6d9d2c35ca8bfd364c564c15b5b5574d
Temitope Oluwafemi <temitope.e.oluwafemi@intel.com>
2022-09-15 16:02:02 -0400

    Removed stray merge conflict message

--- gprofiler/profilers/__init__.py
+++ gprofiler/profilers/__init__.py
@@ -8,7 +8,6 @@ if not is_windows():
     from gprofiler.profilers.php import PHPSpyProfiler
     from gprofiler.profilers.ruby import RbSpyProfiler
 from gprofiler.profilers.python_ebpf import PythonEbpfProfiler
->>>>>>> f5f809b... Moved implementation for PythonEbpfProfiler and PythonEbpfError to its on file. Addressed other issues raised during the PR review
 
 __all__ = (["JavaProfiler", "SystemProfiler", "PHPSpyProfiler", "PythonProfiler", "RbSpyProfiler", "DotnetProfiler"]\
            if not is_windows()\
